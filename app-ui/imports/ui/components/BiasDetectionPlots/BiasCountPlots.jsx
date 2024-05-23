6a22ff2129feb6d3159589aefaf85647057607ca
Aditya Bhattacharya <adib0073@gmail.com>
2024-05-23 13:47:57 +0200

    added threshold lines and arrows for Count Plots

--- app-ui/imports/ui/components/BiasDetectionPlots/BiasCountPlots.jsx
+++ app-ui/imports/ui/components/BiasDetectionPlots/BiasCountPlots.jsx
@@ -14,8 +14,44 @@ const labelWrapper = (value) => {
     return wrappedArray;
 };
 
+// only active function for mouse move
+function handleMouseMove(chart, mousemove, cov_thres) {
+    chart.update('none');
 
-export const BiasCountPlots = ({ y_values, x_values, coverage, rr}) => {
+    const { ctx, chartArea: { top, bottom, left, right, width, height }, scales: { x, y } } = chart;
+
+    let xVal = x.getValueForPixel(mousemove.offsetX);
+    let yVal = y.getValueForPixel(mousemove.offsetY);
+
+    const fontHeight = 0.25 * height;
+    ctx.font = `bolder ${fontHeight / 2}px Roboto`;
+
+    if (yVal > cov_thres) {
+        chart.update('none');
+        ctx.fillStyle = '#449231';
+        ctx.fillText('High Coverage', width / 6, y.getPixelForValue(cov_thres) - top)
+        ctx.fillStyle = 'rgba(0, 200, 0, 0.2)';
+        ctx.fillRect(left, top, width, y.getPixelForValue(cov_thres) - top)
+        ctx.restore();
+    }
+    else {
+        chart.update('none');
+        ctx.fillStyle = '#D64242';
+        ctx.fillText('Low Coverage', width / 6, y.getPixelForValue(0) - top)
+        ctx.fillStyle = 'rgba(200, 0, 0, 0.2)';
+        ctx.fillRect(left, y.getPixelForValue(cov_thres), width, height / 2)
+        ctx.restore();
+    }
+
+}
+
+// only active function for mouse leave element
+function handleMouseOut(chart, event) {
+    chart.update('none');
+    chart.update();
+}
+
+export const BiasCountPlots = ({ y_values, x_values, coverage, rr, cov_thres }) => {
 
     let data = {
         labels: labelWrapper(x_values),
@@ -25,15 +61,15 @@ export const BiasCountPlots = ({ y_values, x_values, coverage, rr}) => {
                 data: y_values,
                 pointRadius: 0,
                 fill: true,
-                backgroundColor: ["#67A3FF", "#244CB1"],
-                borderColor: ["#67A3FF", "#244CB1"],
+                backgroundColor: ["#244CB1", "#67A3FF"],
+                borderColor: ["#244CB1", "#67A3FF"],
                 barPercentage: 0.6,
                 categoryPercentage: 0.8,
                 //maxBarThickness: 20,
                 datalabels: {
                     anchor: 'end',
                     align: 'top',
-                    offset: 8,
+                    offset: 0,
                 }
             },
         ],
@@ -48,7 +84,7 @@ export const BiasCountPlots = ({ y_values, x_values, coverage, rr}) => {
             datalabels: {
                 color: "#000",
                 formatter: function (value, context) {
-                    return "RR: "+ rr[context.dataIndex] + "\n" +"coverage :" + coverage[context.dataIndex];
+                    return "RR: " + rr[context.dataIndex] + "\n" + "Coverage :" + coverage[context.dataIndex];
                 },
                 textAlign: 'center',
                 font: function (context) {
@@ -84,7 +120,7 @@ export const BiasCountPlots = ({ y_values, x_values, coverage, rr}) => {
                 }
             },
         },
-        layout:{
+        layout: {
             padding: {
                 left: 10,
                 right: 10,
@@ -100,7 +136,7 @@ export const BiasCountPlots = ({ y_values, x_values, coverage, rr}) => {
                     drawTicks: false,
                 },
                 min: 0,
-                max: Math.max.apply(Math, y_values) * 2,
+                max: Math.max.apply(Math, y_values) * 1.5,
                 ticks: {
                     padding: 0,
                     color: "#000000",
@@ -115,7 +151,7 @@ export const BiasCountPlots = ({ y_values, x_values, coverage, rr}) => {
                     font: {
                         size: "12vh"
                     }
-                  }
+                }
             },
             x: {
                 display: true,
@@ -137,6 +173,68 @@ export const BiasCountPlots = ({ y_values, x_values, coverage, rr}) => {
         },
     };
 
+
+
+    const onMove = (event) => {
+        const { current: chart } = chartRef;
+        if (!chart) {
+            return;
+        }
+        handleMouseMove(chart, event.nativeEvent, cov_thres);
+    }
+    const onOut = (event) => {
+        const { current: chart } = chartRef;
+        if (!chart) {
+            return;
+        }
+        handleMouseOut(
+            chart,
+            event.nativeEvent);
+    }
+    // plugin block
+    const thresholdLine = {
+        id: 'thresholdLine',
+        afterDatasetDraw(chart, args, option) {
+            const { ctx, chartArea: { top, right, bottom, left, width, height }, scales: { x, y } } = chart;
+            ctx.save();
+            const fontHeight = 0.25 * height;
+            ctx.font = `bolder ${fontHeight / 2}px Roboto`;
+            ctx.fillStyle = '#D64242';
+            ctx.textAlign = 'right';
+            ctx.fillText('Threshold:', right, y.getPixelForValue(cov_thres) - top - (0.1 * height))
+            ctx.fillText(`${cov_thres}`, right, y.getPixelForValue(cov_thres) - top)
+            // Threshold Line   
+            ctx.strokeStyle = "#D64242";
+            //ctx.setLineDash([5, 10]);
+            ctx.strokeRect(left, y.getPixelForValue(cov_thres), width, 0);
+            ctx.restore();
+        },
+        afterDraw(chart, args, option) {
+            const { ctx, chartArea: { top, right, bottom, left, width, height }, scales: { x, y } } = chart;
+            ctx.save();
+
+            const arrowWidth = 3;
+            //Arrow heads - top
+            ctx.beginPath();
+            ctx.moveTo(left, top - 5);
+            ctx.lineTo(left - arrowWidth, top);
+            ctx.lineTo(left + arrowWidth, top);
+            ctx.fillStyle = 'black';
+            ctx.fill();
+            ctx.closePath();
+            ctx.restore();
+            //Arrow heads - right
+            ctx.beginPath();
+            ctx.moveTo(right, bottom - arrowWidth);
+            ctx.lineTo(right, bottom + arrowWidth);
+            ctx.lineTo(right + 5, bottom);
+            ctx.fillStyle = 'black';
+            ctx.fill();
+            ctx.closePath();
+            ctx.restore();
+        }
+    }
+
     data.labels = labelWrapper(x_values);
 
     data.datasets[0].data = y_values;
@@ -149,9 +247,10 @@ export const BiasCountPlots = ({ y_values, x_values, coverage, rr}) => {
             options={options}
             ref={chartRef}
             redraw={true}
-            //onMouseDown={onDown}
-            //onMouseUp={onUp}
-            plugins={[ChartDataLabels]}
+            onMouseMove={onMove}
+            onMouseLeave={onOut}
+            onMouseOut={onOut}
+            plugins={[ChartDataLabels, thresholdLine]}
         />
     </div>);
 };
