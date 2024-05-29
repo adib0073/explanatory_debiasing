1954a1c431f8cf7abff4a4f259bc22799023eb97
Aditya Bhattacharya <adib0073@gmail.com>
2024-05-29 12:38:36 +0200

    solved scroll x and column fixing problems

--- app-ui/imports/ui/components/DataGenController/CustomTableComponent.jsx
+++ app-ui/imports/ui/components/DataGenController/CustomTableComponent.jsx
@@ -151,8 +151,8 @@ export const CustomTableComponent = (
         {
             title: 'Edit',
             dataIndex: 'edit',
-            width: '10%',
             fixed: 'left',
+            width: "5vw",
             render: (_, record) => {
                 const editable = isEditing(record);
                 return editable ? (
@@ -179,7 +179,7 @@ export const CustomTableComponent = (
         {
             title: 'Confidence',
             dataIndex: 'conf',
-            width: '15%',
+            ellipsis: true,
             editable: false,
             render: (_, record) => {
                 let color = 'red'
@@ -200,49 +200,49 @@ export const CustomTableComponent = (
         {
             title: 'Prediction',
             dataIndex: 'pred',
-            width: '15%',
             editable: false,
+            ellipsis: true,
         },
         {
             title: 'Var1',
             dataIndex: 'name',
-            width: '15%',
             editable: true,
+            ellipsis: true,
         },
         {
             title: 'Age',
             dataIndex: 'age',
-            width: '15%',
             editable: true,
+            ellipsis: true,
         },
         {
             title: 'Var2',
             dataIndex: 'address',
-            width: '15%',
             editable: true,
+            ellipsis: true,
         },
         {
             title: 'Var3',
             dataIndex: 'address',
-            width: '15%',
             editable: true,
+            ellipsis: true,
         },
         {
             title: 'Var4',
             dataIndex: 'address',
-            width: '15%',
             editable: true,
+            ellipsis: true,
         },
         {
             title: 'Remove',
             dataIndex: 'remove',
-            width: '15%',
+            width: "6vw",
             fixed: 'right',
             render: (_, record) =>
                 data.length >= 1 ? (
                     <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
                         <a>
-                            <span style={{ color: redFont, fontSize : "1.5vh", textDecoration: "underline"  }}>
+                            <span style={{ color: redFont, fontSize: "1.5vh", textDecoration: "underline" }}>
                                 Remove
                             </span>
                         </a>
@@ -268,7 +268,66 @@ export const CustomTableComponent = (
         };
     });
 
+    /*
+    <Form form={form} component={false}>
 
+        <Table
+            components={{
+                body: {
+                    cell: EditableCell,
+                },
+            }}
+            bordered
+            dataSource={data}
+            columns={mergedColumns}
+            rowClassName="editable-row"
+            style={
+                { fontSize: "1.0vh" }
+            }
+            scroll={{
+                x: "max-content",
+                y: "30vh"
+            }}
+            pagination={{
+                position: ["bottomLeft"],
+                size: 'small',
+                pageSize: 5,
+
+            }}
+        />
+    </Form>
+    */
+    /*
+    const col = []
+    const colN = 15
+    for (let i = 0; i < colN; i++) {
+        col.push({
+            title: `Col ${i + 1}`,
+            dataIndex: `Col_${i + 1}`,
+            fixed: (i < 2) ? 'left' : (i === colN - 1) ? 'right' : null
+        })
+    }
+
+    const dataS = []
+    const dsN = 20
+    for (let i = 0; i < dsN; i++) {
+        const rowdata = {}
+        for (let colIndex = 0; colIndex < colN; colIndex++) {
+            rowdata[`Col_${colIndex + 1}`] = `R${i + 1}C${colIndex + 1}`
+        }
+        dataS.push(rowdata)
+    }
+    <Table
+        style={{ maxWidth: "45vw" }}
+        bordered={true}
+        columns={col}
+        dataSource={dataS}
+        scroll={{
+            x: true,
+            y: "30vh"
+        }}
+    />
+    */
     return (
         <>
             <Form form={form} component={false}>
@@ -284,11 +343,14 @@ export const CustomTableComponent = (
                     columns={mergedColumns}
                     rowClassName="editable-row"
                     style={
-                        { fontSize: "1.0vh" }
+                        {
+                            fontSize: "1.0vh",
+                            maxWidth: "45vw"
+                        }
                     }
                     scroll={{
-                        y: "30vh",
-                        x: "30vw"
+                        x: "max-content",
+                        y: "30vh"
                     }}
                     pagination={{
                         position: ["bottomLeft"],
